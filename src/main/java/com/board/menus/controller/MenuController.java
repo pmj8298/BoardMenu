6cf70d2057e2eee787a100ee0340651191eb3b5f
pmj8298 <pmj8298@naver.com>
2024-03-21 15:37:12 +0900

    BoardMenu 추가,조회,삭제2

--- src/main/java/com/board/menus/controller/MenuController.java
+++ src/main/java/com/board/menus/controller/MenuController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.board.menus.domain.MenuVo;
 import com.board.menus.mapper.MenuMapper;
@@ -61,6 +62,21 @@ public class MenuController {
 	
 	// 메뉴삭제 /Menus/Delete
 	@RequestMapping("/Delete")
+	@ResponseBody
+	public String delete(MenuVo menuVo) {
+		menuMapper.deleteMenu(menuVo);
+		
+		String html = "<script>";
+		html += "alert('삭제되었습니다');";
+		html += "location.href='/Menus/List';";
+		html += "</script>";
+		
+		return html;
+	}	
+	
+	/*
+	// 메뉴삭제 /Menus/Delete
+	@RequestMapping("/Delete")
 	public String delete (MenuVo menuVo, Model model) {
 		// MENU03을 삭제
 		menuMapper.deleteMenu(menuVo);
@@ -72,6 +88,6 @@ public class MenuController {
 		
 		//이동할 파일
 		//return "menus/list";
-	}
+	}*/
 	
 }
